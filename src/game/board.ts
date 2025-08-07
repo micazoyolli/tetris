f74000a9a358970a12b3976b10b11f949fe1b8ac
Nadia AB <nadia.aragon@the-cocktail.com>
2025-08-06 19:36:42 -0600

    build: dist actualizado correctamente

--- src/game/board.ts
+++
@@ -1,8 +0,0 @@
-export const ROW = 20;
-export const COL = 10;
-export const SQ = 30;
-export const VACANT = '#1b1b1b';
-
-export function createBoard() {
-  return Array.from({ length: ROW }, () => Array(COL).fill(VACANT));
-}
